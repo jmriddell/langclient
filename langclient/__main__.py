3dfcbf76c8d0bc3cd1682a9dd2843a974c5f8cfd
Juan Molina Riddell <j.molina.riddell@gmail.com>
2023-11-08 22:16:32 -0300

    feat: load key from arg or file

--- langclient/__main__.py
+++ langclient/__main__.py
@@ -1,11 +1,12 @@
 """Interactive demo for langclient."""
 
 from itertools import accumulate
+import argparse
 from typing import Iterable, Callable
 import readline  # noqa: F401
 
 from chat_functions import stream_chat
-from openai_auth import use_key
+from openai_auth import use_key, read_key_from_file
 from models import Message, Role
 
 
@@ -40,9 +41,28 @@ def _print_intercept(iter: Iterable):
     return _side_effect(_print_inline, iter)
 
 
-def main():
+def _read_key_from_file_if_path(path: str | None) -> str | None:
+    """Read the key from the given path if it exists.
+
+    Args:
+        path (str | None): The path to read from.
+        open (type(open), optional): The open function to use. Defaults to open.
+
+    Returns:
+        str | None: The key if it exists, otherwise None.
+    """
+    if path is None:
+        return None
+    return read_key_from_file(path)
+
+
+def main(pre_provided_api_key: str | None = None, api_key_file_path: str | None = None):
     # Prompt the user for their API key
-    api_key = input("Please enter your API key: ")
+    api_key = (
+        pre_provided_api_key
+        or _read_key_from_file_if_path(api_key_file_path)
+        or input("Please enter your API key: ")
+    )
     print()
 
     # Decorate the stream_chat function with the given key
@@ -66,4 +86,21 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # read args
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--api-key",
+        "-k",
+        type=str,
+        help="The api key to use.",
+    )
+    parser.add_argument(
+        "--api-key-file",
+        "-f",
+        type=str,
+        help="The file to read the api key from.",
+    )
+    args = parser.parse_args()
+
+    # run main
+    main(args.api_key, args.api_key_file)
