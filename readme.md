84d5cd86831dc495fd065abd33c9d8346849a372
Juan Molina Riddell <j.molina.riddell@gmail.com>
2024-01-14 16:34:26 -0300

    add readme and setup

---
+++ readme.md
@@ -0,0 +1,23 @@
+# langclient
+
+## Installation
+
+### Prerequisites
+
+* git
+* python >= 3.11
+
+### Install
+
+```bash
+pip install git+https://github.com/jmriddell/langclient.git#main
+```
+
+## Usage
+
+```bash
+langclient [--api-key-file <api-key-file> | --api-key <api-key>]
+```
+Api key file should contain a single line with the api key.
+
+If api key not provided in any way, the program will prompt for it.
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+with open("readme.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="langclient",
+    version="0.1",
+    description="Simple OpenAI language repl client",
+    author="Juan Molina Riddell",
+    author_email="jmriddell@protonmail.ch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jmriddell/langclient",
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Natural Language :: English",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Utilities",
+    ],
+    packages=find_packages(),
+    intall_requires=["openai", "pydantic"],
+    entry_points=dict(console_scripts=["langclient=langclient"]),
+)
