7256e1d02adf4951471303aa197fcb5d2ded39c7
Paulo Ferm <paulo.fzmz@proton.me>
2024-01-24 15:39:12 -0300

    user_input() to chat_functions, call files with <>

--- langclient/chat_functions.py
+++ langclient/chat_functions.py
@@ -1,9 +1,10 @@
 from typing import Iterable
+from re import findall
 
 from openai import OpenAI
 from openai.types.chat import ChatCompletionChunk
 
-from langclient.models import Message
+from langclient.models import Message, Role
 from langclient.data_transformations import message_to_dict
 
 
@@ -34,3 +35,35 @@ def stream_chat(
     not_none_content = filter(lambda content: content is not None, deltas_content)
 
     return not_none_content
+
+
+def _parse_file_content(message: str) -> dict:
+    "Match file mentions in <angle brackets> on message and parse them as content"
+    pattern = r'<(.*?)>'
+    files = findall(pattern, message)
+    
+    def _segment_file_content(file):
+        content = f"START {file} CONTENT:  "
+
+        try:
+            with open(file) as data:
+                content += data.read()
+        except FileNotFoundError:
+            return f"FILE {file} NOT ACCESSIBLE"
+
+        content += f"  END {file} CONTENT"
+        
+        return content
+
+    contents = [ _segment_file_content(file) for file in files ]
+
+    return "  ".join(contents)
+
+
+def user_input() -> Iterable[Message]:
+    """Get user input for a chat."""
+
+    while True:
+        input_message = input("You:\n")
+        input_message += _parse_file_content(input_message)
+        yield Message(role=Role.USER, content=input_message)
