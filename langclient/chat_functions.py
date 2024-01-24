f5fc8835d1aee6f480e3095e38cb4f0d162ad680
Paulo Ferm <paulo.fzmz@proton.me>
2024-01-24 16:28:58 -0300

    rev: fix black compliance

--- langclient/chat_functions.py
+++ langclient/chat_functions.py
@@ -39,7 +39,7 @@ def stream_chat(
 
 def _parse_file_content(message: str) -> dict:
     "Match file mentions in <angle brackets> on message and parse them as content"
-    pattern = r'<(.*?)>'
+    pattern = r"<(.*?)>"
     files = findall(pattern, message)
     
     def _segment_file_content(file):
@@ -52,10 +52,10 @@ def _parse_file_content(message: str) -> dict:
             return f"FILE {file} NOT ACCESSIBLE"
 
         content += f"  END {file} CONTENT"
-        
+
         return content
 
-    contents = [ _segment_file_content(file) for file in files ]
+    contents = [_segment_file_content(file) for file in files]
 
     return "  ".join(contents)
 
--- langclient/start_menu.py
+++ langclient/start_menu.py
@@ -7,10 +7,10 @@ def select_language_model() -> str:
         inquirer.List(
             'model',
             message="Select a model:",
-            choices=["gpt-3.5-turbo-1106", "gpt-4-1106-preview", "gpt-4"]
+            choices=["gpt-3.5-turbo-1106", "gpt-4-1106-preview", "gpt-4"],
         ),
     ]
 
     answer = inquirer.prompt(select_model)
-    
+
     return answer["model"]
