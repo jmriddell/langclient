b6e9cd1ef1a20f0bf3df4bd8864d0c82edb14a9f
Juan Molina Riddell <j.molina.riddell@gmail.com>
2024-01-24 14:04:42 -0300

    ref: fix black compliance

--- langclient/__main__.py
+++ langclient/__main__.py
@@ -136,13 +136,10 @@ def interactve_chat(
     )
 
     select_model = [
-        inquirer.List('model',
+        inquirer.List(
+            "model",
             message="Select a model:",
-            choices=[
-                "gpt-3.5-turbo-1106", 
-                "gpt-4-1106-preview", 
-                "gpt-4"
-            ]
+            choices=["gpt-3.5-turbo-1106", "gpt-4-1106-preview", "gpt-4"],
         ),
     ]
     answer = inquirer.prompt(select_model)
