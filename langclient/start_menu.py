c81546fe605b247a57b8ece8d1a99fcb2a237664
Paulo <paulo.fzmz@proton.me>
2024-02-08 15:35:00 -0300

    Updated models at 2402

--- langclient/start_menu.py
+++ langclient/start_menu.py
@@ -7,7 +7,13 @@ def select_language_model() -> str:
         inquirer.List(
             "model",
             message="Select a model:",
-            choices=["gpt-3.5-turbo-1106", "gpt-4-1106-preview", "gpt-4"],
+            choices=[
+                "gpt-3.5-turbo-0125",
+                "gpt-4-0125-preview",
+                "gpt-4-1106-preview",
+                "gpt-4",
+                "gpt-4-32k",
+            ],
         ),
     ]
 
