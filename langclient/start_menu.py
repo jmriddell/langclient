bf88c4fc35dbc7ddb7d56f7e03abd73d59c962c3
Paulo Ferm <paulo.fzmz@proton.me>
2024-02-09 12:17:43 -0300

    User name input at start

--- langclient/start_menu.py
+++ langclient/start_menu.py
@@ -1,4 +1,35 @@
 import inquirer
+import json
+from os.path import exists, expanduser
+
+default_user_config_file = expanduser("~/.langclient/user_config.json")
+
+
+def save_user_name(user_name: str, file_path: str = default_user_config_file):
+    """Save user name on user config json file"""
+    with open(file_path, "w") as file:
+        json.dump({"name": user_name}, file)
+
+
+def ask_user_name() -> str:
+    ask_name = [inquirer.Text("user_name", message="Your Name")]
+
+    answer = inquirer.prompt(ask_name)
+
+    return answer["user_name"]
+
+
+def user_name(file_path: str = default_user_config_file) -> str:
+    """Name user to display on dialog"""
+    if not exists(file_path):
+        user_name = ask_user_name()
+        save_user_name(user_name)
+
+    with open(default_user_config_file, "r") as file:
+        user_config = json.load(file)
+        user_name = user_config["name"]
+
+    return user_name
 
 
 def select_language_model() -> str:
