d49e509b72801b389d33451fd4f9e83ad170a7ed
Paulo Ferm <paulo.fzmz@proton.me>
2024-02-13 17:15:04 -0300

    Version 0.3.4: Usage-stats

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DEPENDENCIES = ["openai", "pydantic", "inquirer", "colorama", "pyreadline3", "ti
 
 setup(
     name="langclient",
-    version="0.3.3",
+    version="0.3.4",
     description="Simple OpenAI language repl client",
     author="Juan Molina Riddell",
     author_email="jmriddell@protonmail.ch",
